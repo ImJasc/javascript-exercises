625d9db854c5319a90268a85c5c49796c04299e9
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-14 10:47:07 -0700

    solved test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    const arrayToBeRemoved = arguments[0];
+    for (let indexArray = 0; indexArray < arrayToBeRemoved.length; indexArray++) {
+        for (let indexArguments = 0; indexArguments < arguments.length; indexArguments++) {
+            if (arrayToBeRemoved[indexArray] === arguments[indexArguments]) {
+                arrayToBeRemoved.splice(indexArray, 1);
+            }
+            
+        }        
+    }
+    return arrayToBeRemoved
 };
 
 // Do not edit below this line
