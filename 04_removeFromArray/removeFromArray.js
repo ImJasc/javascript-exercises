f3b866810301997a3efb03062adb62b47e0e48b5
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-14 10:52:06 -0700

    change the fors

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,10 @@
 const removeFromArray = function() {
     const arrayToBeRemoved = arguments[0];
-    for (let indexArray = 0; indexArray < arrayToBeRemoved.length; indexArray++) {
-        for (let indexArguments = 0; indexArguments < arguments.length; indexArguments++) {
+    for (let indexArguments = 0; indexArguments < arguments.length; indexArguments++) {
+        for (let indexArray = 0; indexArray < arrayToBeRemoved.length; indexArray++) {
             if (arrayToBeRemoved[indexArray] === arguments[indexArguments]) {
                 arrayToBeRemoved.splice(indexArray, 1);
-            }
-            
+            }            
         }        
     }
     return arrayToBeRemoved
