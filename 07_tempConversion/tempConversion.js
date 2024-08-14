c8a3546ad07d8859fe84fb012428dea64d48c7db
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-14 12:46:20 -0700

    solved test #1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemperature = (temperature - 32) * (5/9);
+  parseFloat(convertedTemperature.toFixed(1));
+  return convertedTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemperature = (temperature * (9/5)) + 32;
+  parseFloat(convertedTemperature.toFixed(1));
+  return convertedTemperature;
 };
 
 // Do not edit below this line
