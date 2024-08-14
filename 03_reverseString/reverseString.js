fae58ea39e674ddcb745cefe6e33b748782e694f
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-13 21:04:20 -0700

    solved the excercise test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const elements = [];
+    let reversedString = '';
+    for (let index = 0; index < input.length; index++) {
+        elements[index] = input.charAt(index);                    
+    }
+    
+    for (let index = elements.length-1; index >= 0; index--) {
+        reversedString += elements[index];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
