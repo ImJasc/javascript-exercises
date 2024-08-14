63e6fc2e9c225c47e7c6da153a2a8a3dce55486a
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-13 20:26:24 -0700

    removed the skip for the 5th test, alter the solution to return error when using a negative number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(str, looptime) {
     let finalstring = ''
-    for (let index = 0; index < looptime; index++) {        
-        finalstring = finalstring+str;
-    }
-    return finalstring;
+    if (looptime < 0) {
+        return 'ERROR';
+    }else{
+        for (let index = 0; index < looptime; index++) {        
+            finalstring = finalstring+str;
+        }
+        return finalstring;
+    }    
 };
 
 // Do not edit below this line
