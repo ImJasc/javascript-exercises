a95a16f3cba41d848aa8c25addd7117a133519a5
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-13 20:21:16 -0700

    solved the excercise, worked for test one and two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, looptime) {
+    let finalstring = ''
+    for (let index = 0; index < looptime; index++) {        
+        finalstring = finalstring+str;
+    }
+    return finalstring;
 };
 
 // Do not edit below this line
