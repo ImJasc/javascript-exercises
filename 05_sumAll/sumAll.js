201d5513b469038a17b871ad03dd425886410296
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-14 11:38:46 -0700

    solved test #1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(valueOne, valueTwo) {
+    let sumAllValue = 0;
+    if (Number.isInteger(valueOne+valueTwo)) {
+        if (valueOne < 0 || valueTwo < 0) {
+            return "ERROR";
+        }else{
+            if (valueOne > valueTwo) {
+                sumAllValue = ((valueOne+valueTwo) * (valueOne/2));
+            }else{
+                sumAllValue = ((valueOne+valueTwo) * (valueTwo/2));
+            }
+            return sumAllValue;            
+        }
+    }else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
