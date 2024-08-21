2909a4bdaaa071d8110719a3a644bed9043e9b35
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-20 20:23:43 -0700

    added solution to all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (Str) {
+    let re = /[\W_]/g;
+    let puntuationless = Str.toLowerCase().replace(re ,"");
+    let reversedString = puntuationless.split("").reverse().join("");
+     
+    return puntuationless === reversedString;
 };
 
 // Do not edit below this line
