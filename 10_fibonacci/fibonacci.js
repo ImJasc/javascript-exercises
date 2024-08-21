f0946f002fbc627250e95837afe4ecd482007a7d
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-20 20:47:36 -0700

    added solution to all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }else if (parseInt(number) === 0){
+        return 0;
+    }
 
+    let value = [1, 1];
+    for (let index = 2; index < parseInt(number); index++) {
+        value[index] = value[index-2] + value[index-1];        
+    }
+
+    return value[number-1];
 };
 
 // Do not edit below this line
