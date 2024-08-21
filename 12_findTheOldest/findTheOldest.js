721334bd92c884ef685a9f5b865679afd7f10cee
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-21 11:30:08 -0700

    added solution to all test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const thisYear = new Date().getFullYear();
+    
+    return arr.reduce((res, person) => {
+        const age = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+        return age > res.age ? { person, age } : res;
+    }, { person: null, age: 0 }).person;      
+    
 };
 
 // Do not edit below this line
